4ba6f73abacd9a17083e14a96699369400ce2f55
Donato Renzulli <donren.99@gmail.com>
2020-11-11 11:04:58 +0100

    Package

---
+++ public/sketch.js
@@ -0,0 +1,15 @@
+function preload(){
+  // put preload code here
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  background('yellow')
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  fill(20)
+  ellipse(mouseX, mouseY, 20)
+}
